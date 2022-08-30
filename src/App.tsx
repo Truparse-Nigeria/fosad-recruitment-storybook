cfd67c1710cf66c8d34396106df4c98c18b361f9
Nzaki Michael <ekisnzaki@gmail.com>
2022-08-30 10:59:26 +0100

    initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from "components/Button";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-3xl font-bold underline bg-red-700">
+      Hello world!
+    </h1>
+    <Button label="Button007"/>
     </div>
-  );
+  )
 }
-
-export default App;
