fa167c89d78b111ecc4f801abccd58a513f2d9c4
Nzaki Michael <ekisnzaki@gmail.com>
2022-09-02 07:18:39 +0100

    Feat: Added Avatar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing:{
+        '17':'4.25rem'
+      }
+    },
   },
   plugins: [],
 }
