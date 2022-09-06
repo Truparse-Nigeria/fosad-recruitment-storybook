80d94aac58bc87ac359a0d00bce28179013e3e2d
Nzaki Michael <ekisnzaki@gmail.com>
2022-09-06 22:42:16 +0100

    Switching

--- src/components/SampleComponent/index.tsx
+++ src/components/SampleComponent/index.tsx
@@ -1,5 +1,5 @@
 // import Pill from 'components/Pills'
-import Switch from 'components/Switch'
+import Button from 'components/Button'
 import React, { ReactElement } from 'react'
 
 interface SampleProps {
@@ -15,7 +15,7 @@ function SampleComponent({ label, textColor,backgroundColor, children }: SampleP
             <h1>{label}</h1>
             <div>{children}</div>
             {/* <Pill label='Active' type='offer' size='medium' /> */}
-            <Switch />
+            <Button label='Button' size='large' color='secondary'/>
         </div>
     )
 }
