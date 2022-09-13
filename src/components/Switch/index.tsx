c14c9ba53729821e53bf1431b8676be7c850ffdb
= <=>
2022-09-13 08:35:15 +0100

    Updated Version with Switch

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -1,21 +1,23 @@
+import { ComponentSize } from "components/types";
 import React, { ChangeEventHandler } from "react";
 
 interface SwitchProps {
   id?: string;
-  value?:string;
+  value?: string;
   checked?: boolean;
-  onChange?: ChangeEventHandler<HTMLInputElement>; 
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+  size: ComponentSize;
 }
 
-function Switch({ id,value, checked, onChange }: SwitchProps) {
+function Switch({ id, value, checked, size, onChange }: SwitchProps) {
   const randId = `${Math.floor(Math.random() * 1000)}-switch-${Math.floor(
     Math.random() * 1000
-  )}`; 
+  )}`;
 
   return (
-    <div className="switch w-2/4">
-      <input id={id?id:randId} value={value} checked={checked} onChange={onChange} className="switch-input" type="checkbox" />
-      <label htmlFor={id?id:randId} className="switch-label"></label>
+    <div className="switch">
+      <input id={id ? id : randId} value={value} checked={checked} onChange={onChange} className="switch-input" type="checkbox" />
+      <label htmlFor={id ? id : randId} className={`switch-label ${size === "small" ? "switch-label-sm" : size === "medium" ? "switch-label-md" : size === "large" ? "switch-label-lg" : "switch-label-default"}`}></label>
     </div>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,7 @@
-export { default as SampleComponent } from './SampleComponent';
 export { default as Avatar } from 'components/Avatar';
+export { default as Icon } from 'components/Icon';
+export { default as Pill } from 'components/Pills';
+export { default as Checkbox } from 'components/Checkbox';
+export { default as Switch } from 'components/Switch';
+export { default as TextArea } from 'components/TextArea';
+// export { default as Avatar } from 'components/Avatar';
